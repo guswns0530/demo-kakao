8d03e31e1722ce6e8502165b3e1d9798d1253425
박현준 <78728441+guswns0530@users.noreply.github.com>
2022-09-16 09:45:58 +0900

    ~

--- src/main/java/com/oauth2/sample/web/security/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/oauth2/sample/web/security/jwt/JwtAuthenticationEntryPoint.java
@@ -1,7 +1,16 @@
 package com.oauth2.sample.web.security.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.jfr.ContentType;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.*;
+import org.springframework.security.config.web.servlet.headers.ContentTypeOptionsDsl;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +18,38 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    
+
     @Override
-    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
-        log.error("Responding with unauthorized error. Message - {}", e.getMessage());
-        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getLocalizedMessage());
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException exception) throws IOException, ServletException {
+        httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+        String errorMessage;
+
+        if (exception instanceof BadCredentialsException) {
+            errorMessage = "아이디 또는 비밀번호가 맞지 않습니다. 다시 확인해 주세요.";
+        } else if (exception instanceof InternalAuthenticationServiceException) {
+            errorMessage = "내부적으로 발생한 시스템 문제로 인해 요청을 처리할 수 없습니다. 관리자에게 문의하세요.";
+        } else if (exception instanceof UsernameNotFoundException) {
+            errorMessage = "계정이 존재하지 않습니다. 회원가입 진행 후 로그인 해주세요.";
+        } else if (exception instanceof AuthenticationCredentialsNotFoundException) {
+            errorMessage = "인증 요청이 거부되었습니다. 관리자에게 문의하세요.";
+        } else {
+            errorMessage = "알 수 없는 이유로 로그인에 실패하였습니다 관리자에게 문의하세요.";
+        }
+
+        log.error("Responding with unauthorized error. Message - {}", errorMessage);
+
+        try(OutputStream os = httpServletResponse.getOutputStream()) {
+            ObjectMapper om = new ObjectMapper();
+            om.writeValue(os, errorMessage);
+            os.flush();
+        }
     }
 }
--- src/main/java/com/oauth2/sample/web/security/oauth2/handler/OAuth2AuthenticationFailureHandler.java
+++ src/main/java/com/oauth2/sample/web/security/oauth2/handler/OAuth2AuthenticationFailureHandler.java
@@ -23,8 +23,6 @@ public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationF
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
 
-        System.out.println("exception = " + exception);
-
         String targetUrl = CookieUtils.getCookie(request, HttpCookieOAuth2AuthorizationRequestRepository.REDIRECT_URI_PARAM_COOKIE_NAME)
                 .map(Cookie::getValue)
                 .orElse(("/"));
