610dd46b79b9319e16d05b505473b7e3f93cd08a
박현준 <78728441+guswns0530@users.noreply.github.com>
2022-10-17 10:04:44 +0900

    ~

---
+++ kakao-server/src/main/java/com/oauth2/sample/domain/file/dto/File.java
@@ -0,0 +1,9 @@
+package com.oauth2.sample.domain.file.dto;
+
+public class File {
+    private String fileId;
+    private String email;
+    private String fileName;
+    private String originalName;
+    private String originalExt;
+}
