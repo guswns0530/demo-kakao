7932bb26678f91858afb86cd89d2018c7a3e2076
박현준 <78728441+guswns0530@users.noreply.github.com>
2022-10-26 01:13:10 +0900

    ~

--- kakao-front/src/pages/LoginPage.js
+++ kakao-front/src/pages/LoginPage.js
@@ -8,22 +8,28 @@ import LoginForm from "../containers/auth/LoginForm";
 
 const LoginPage = () => {
     return (
-        <section id={style.login_popup}>
-            <div className={style.lp_logo}>
-                <object data={logo} alt="KAKAO_LOGO">
-                    <img alt={"logo"} src={logo}></img>
-                </object>
+        <div className={style.account_comm}>
+            <div className={style.account_wrap}>
+                <section id={style.login_popup}>
+                    <div className={style.login_wrap}>
+                        <div className={style.lp_logo}>
+                            <object data={logo} alt="KAKAO_LOGO">
+                                <img alt={"logo"} src={logo}></img>
+                            </object>
+                        </div>
+                        <LoginForm/>
+                        <div className={style.account}>
+                            <Link to={'/register'}>회원가입</Link>
+                            <div className={style.account_more}>
+                                <Link>카카오계정</Link>
+                                <div></div>
+                                <Link>비밀번호 찾기</Link>
+                            </div>
+                        </div>
+                    </div>
+                </section>
             </div>
-            <LoginForm/>
-            <div className={style.account}>
-                <Link to={'/register'}>회원가입</Link>
-                <div className={style.account_more}>
-                    <Link>카카오계정</Link>
-                    <div></div>
-                    <Link>비밀번호 찾기</Link>
-                </div>
-            </div>
-        </section>
+        </div>
     );
 };
 
--- kakao-front/src/pages/RegisterPage.js
+++ kakao-front/src/pages/RegisterPage.js
@@ -6,16 +6,22 @@ import {Link} from "react-router-dom";
 
 const RegisterPage = () => {
     return (<>
-            <section id={style.register_popup}>
-                <Link to={"/"}>
-                    <div className={style.lp_logo}>
-                        <object data={logo} alt="KAKAO_LOGO">
-                            <img alt={"logo"} src={logo}></img>
-                        </object>
-                    </div>
-                </Link>
-                <RegisterForm/>
-            </section>
+            <div className={style.account_comm}>
+                <div className={style.account_wrap}>
+                    <section id={style.register_popup}>
+                        <div className={style.register_wrap}>
+                            <Link to={"/"}>
+                                <div className={style.lp_logo}>
+                                    <object data={logo} alt="KAKAO_LOGO">
+                                        <img alt={"logo"} src={logo}></img>
+                                    </object>
+                                </div>
+                            </Link>
+                            <RegisterForm/>
+                        </div>
+                    </section>
+                </div>
+            </div>
         </>
     );
 };
