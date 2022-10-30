3ee00f837b6ef7bc9a5b14cfe29623255d358b31
박현준 <78728441+guswns0530@users.noreply.github.com>
2022-10-30 23:21:13 +0900

    ~

--- kakao-server/src/main/java/com/oauth2/sample/domain/file/service/FileService.java
+++ kakao-server/src/main/java/com/oauth2/sample/domain/file/service/FileService.java
@@ -5,15 +5,21 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
 public class FileService {
 
     private final FileRepository fileRepository;
+    private final String uploadPath = "/";
 
     public void init() {
         try {
@@ -41,4 +47,44 @@ public class FileService {
             throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
         }
     }
+
+    public void store(MultipartFile file) {
+        try {
+            if (file.isEmpty()) {
+                throw new Exception("ERROR : File is empty.");
+            }
+            Path root = Paths.get(uploadPath);
+            if (!Files.exists(root)) {
+                init();
+            }
+
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, root.resolve(file.getOriginalFilename()),
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Could not store the file. Error: " + e.getMessage());
+        }
+    }
+
+    public Stream<Path> loadAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Path load(String filename) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Resource loadAsResource(String filename) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void deleteAll() {
+        // TODO Auto-generated method stub
+
+    }
+
 }
