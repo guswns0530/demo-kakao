8a945272971df7e61dcbfee09cbee689fcaf788c
박현준 <78728441+guswns0530@users.noreply.github.com>
2022-11-02 02:04:56 +0900

    ~

--- kakao-front/src/component/util/Svg.js
+++ kakao-front/src/component/util/Svg.js
@@ -1,10 +1,13 @@
 import React from "react";
+import styled from 'styled-components'
 
 const Svg = ({children}) => {
+    const Svg = styled.svg`
+    `
     return (<svg>
-        <fieldset>
+        <foreignObject style={{width: '100%', height: '100%'}}>
             {children}
-        </fieldset>
+        </foreignObject>
     </svg>)
 }
 
